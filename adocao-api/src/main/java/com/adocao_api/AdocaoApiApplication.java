39d602e29c6a38c2bf097a0b38bb2949fc2e2a47
Alexandre Da Silva <alexandrerider@hotmail.com>
2024-08-15 15:24:39 -0300

    first commit

---
+++ adocao-api/src/main/java/com/adocao_api/AdocaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.adocao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdocaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdocaoApiApplication.class, args);
+	}
+
+}
---
+++ adocao-api/src/test/java/com/adocao_api/AdocaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adocao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdocaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
